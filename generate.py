c332439c95cffe2659e913bbdabf0d3d50c063ca
Duncan Sterken <duncan.sterken123@gmail.com>
2019-12-03 14:47:30 +0100

    Stuff

--- generate.py
+++ generate.py
@@ -7,7 +7,6 @@ from configs import model_name
 from configs import train_cfg
 from configs import paths
 from configs import temperature
-import pytumblr
 
 textgen = textgenrnn(
     name=model_name,
@@ -16,11 +15,10 @@ textgen = textgenrnn(
     weights_path=paths['weights_path'],
 )
 
-# temperature = [1.0, 0.5, 0.2, 0.2]
 prefix = None  # if you want each generated text to start with a given seed text
 
 if train_cfg['line_delimited']:
-    n = 1000
+    n = 2
     max_gen_length = 60 if model_cfg['word_level'] else 300
 else:
     n = 1
@@ -40,27 +38,3 @@ generated = textgen.generate(
 output = "\n".join(generated)
 
 print(output)
-
-# with open(gen_file, "w") as text_file:
-#     text_file.write(output)
-
-client = pytumblr.TumblrRestClient(
-  '',
-  '',
-  '',
-  ''
-)
-
-client.create_text(
-    "lexobotus",
-    state="published",
-    # slug="testing-text-posts",
-    # title="Testing",
-    body=output,
-    tags=[
-        "lexobotus",
-        "phandom",
-        "lexxpocalypse",
-        "dev tests",
-    ]
-)
