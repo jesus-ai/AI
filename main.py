859b51377250f90e023c37d3179e47e101bd2b9a
Duncan Sterken <duncan.sterken123@gmail.com>
2020-01-10 10:01:15 +0100

    good stuff

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import urllib.request
+import random
+import math
 
 from tensorflow.keras import utils as keras_utils
 
 from train import Train as Ai
 
+query = '?r={}'.format(math.floor(random.random() * 10000))
+url = 'https://jesus-ai.github.io/scraper/output2.txt'
+
 
 def get_output_hash():
-    response = urllib.request.urlopen('https://jesus-ai.github.io/scraper/output.txt.md5')
+    response = urllib.request.urlopen('{}.md5{}'.format(url, query))
     return response.read().decode('utf-8')
 
 
 # directory = 'data.txt'
 directory = keras_utils.get_file(
     fname='data.txt',
-    origin='https://jesus-ai.github.io/scraper/output.txt',
+    origin=url + query,
     md5_hash=get_output_hash()
 )
 
